6252ee55df8a2528695d7c6b37628174d2ecb4a8
LewisCrombleholme <80609743+LewisCrombleholme@users.noreply.github.com>
2021-03-22 02:32:01 +0000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -68,125 +68,49 @@ function type() {
     }  
  });
 
+// Switch Active
+$(document).ready(function() {
+    scrollActiveLinkControl()
+    activeLinkControl()
+});
 
-// Experience Carousel.
-var itemClassName = "experience__info";
-items = document.getElementsByClassName(".experience__info");
-totalItems = items.length;
-slide = 0;
-moving = true;
-
-function setInitialClasses() {
-    items[totalItems - 1].classList.add("prev");
-    items[0].classList.add("active");
-    items[1].classList.add("next");
+// Switch Active on Click NavBar.
+function activeLinkControl() {
+    $('.nav__links .nav__item a').click(function() {
+        // Removes active class from nav-item.
+        $('.nav__links .nav__item').removeClass('active')
+        // Add active class to clicked item.
+        $(this).closest('.nav__item').addClass('active')
+    });
 }
 
-function setEventListeners() {
-    var next = document.getElementsByClassName("experience__prev");
-    var prev = document.getElementsByClassName("experience__next");
+// Switch Active on Scroll NavBar.
+function scrollActiveLinkControl() {
+    const sections = document.querySelectorAll('section');
+    const navLi = document.querySelectorAll('nav ul li');
 
-    next.addEventListener('click', moveNext);
-    prev.addEventListener('click', movePrev);
-}
+    window.addEventListener('scroll', () => {
+        let current = '';
 
-// Next navigation handler
-function moveNext() {
-    // Check if moving.
-    if(!moving) {
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
 
-        // If it's the last slide, reset to 0, else +1.
-        if(slide === (totalItems - 1)) {
-            slide = 0;
-        }
-        else {
-            slide++;
-        }
-
-        // Move Carousel to update slide
-        moveCarouselTo(slide);
-    }
-}
-
-// Prev navigation handler
-function movePrev() {
-    // Check if moving.
-    if(!moving) {
-
-        // If it's the last slide, reset to 0, else +1.
-        if(slide === (totalItems - 1)) {
-            slide = 0;
-        }
-        else {
-            slide--;
-        }
-
-        // Move Carousel to update slide
-        moveCarouselTo(slide);
-    }
-}
-
-// Disable Interaction
-function disableInteraction() {
-    // set moving to true.
-    moving = true;
-
-    // Timeout
-    setTimeout (function() {
-        moving = false;
-    }, 500);
-}
-
-// Moves Carousel
-function moveCarouselTo(slide) {
-    // Check if moving 
-    if(!moving) {
-
-        // disable interaction
-        disableInteraction();
-
-        // Update the "old" adjacent slides with "new" ones
-        var newPrevious = slide - 1,
-            newNext = slide + 1,
-            oldPrevious = slide - 2,
-            oldNext = slide + 2;
-
-        // test if carousel have more than three items.
-        if((totalItems - 1) > 3) {
-             // Checks and updates if the new slides are out of bounds
-            if (newPrevious <= 0) {
-                oldPrevious = (totalItems - 1);
-            } else if (newNext >= (totalItems - 1)){
-                oldNext = 0;
+            if(pageYOffset >= sectionTop) {
+                current = section.getAttribute('id');
             }
-                // Checks and updates if slide is at the beginning/end
-            if (slide === 0) {
-                newPrevious = (totalItems - 1);
-                oldPrevious = (totalItems - 2);
-                oldNext = (slide + 1);
-            } else if (slide === (totalItems -1)) {
-                newPrevious = (slide - 1);
-                newNext = 0;
-                oldNext = 1;
+        });
+        
+        navLi.forEach( li => {
+            li.classList.remove('active');
+
+            if(li.classList.contains(current)) {
+                li.classList.add('active');
             }
-            // Now we've worked out where we are and where we're going, 
-            // by adding/removing classes we'll trigger the transitions.
-            // Reset old next/prev elements to default classes
-            items[oldPrevious].className = itemClassName;
-            items[oldNext].className = itemClassName;
-            // Add new classes
-            items[newPrevious].className = itemClassName + " prev";
-            items[slide].className = itemClassName + " active";
-            items[newNext].className = itemClassName + " next";
-        }
-    }
-}
+        })
+    });
+};
 
-function initCarousel() {
-    setInitialClasses();
-    setEventListeners();
-    // Set moving to false so that the carousel becomes interactive
-    moving = false;
-}
 
-initCarousel();
+
+
